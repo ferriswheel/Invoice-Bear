05e7527d6de55e46f5b7b164b98b1f341b01d5f9
Kevin Collignon <kcollignon@ubuntu.(none)>
2012-01-24 12:25:37 -0800

    Default rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invoicebear::Application.config.secret_token = '9dc76721b845785310ee3cda933dfabdf0eaa22d80b95e84477beb0bc28ad5a57e6727c14ca55b77db7c7d5e4a274db1d3b62364d9e7e3426020fb1bf49ab7ce'

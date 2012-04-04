938db9d1d214b684939eaba6333d0406e1b9fabc
Kevin Collignon <kevin@kevincollignon.com>
2012-04-03 18:40:11 -0700

    Hopefully fix the InvoiceMailer error

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -5,7 +5,7 @@ class InvoiceMailer < ActionMailer::Base
   
   def invoice_notification(invoice)
 	@invoice = invoice
-	@client = invoice.client
+	@client = @invoice.client
 	mail(:to => client.email, :subject => "New Invoice For You")
   end
 end

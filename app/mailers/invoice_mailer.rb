d0594f1fe434684a51cfaec55455b5fc01d5f43b
Kevin Collignon <kevin@kevincollignon.com>
2012-04-06 19:09:38 -0600

    Dont worry about attaching pdfs yet

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -3,9 +3,8 @@ class InvoiceMailer < ActionMailer::Base
 
   default from: "info@invoicebear.com"
   
-  def invoice_notification(file,invoice)
+  def invoice_notification(invoice)
 	@invoice = invoice
-	attachments["#{invoice.id}.pdf"] = file
 	mail(:to => invoice.client.email, :subject => "New Invoice For You")
   end
 end
---
+++ config/initializers/prawnto_globals.rb
@@ -0,0 +1,5 @@
+PRAWN_DEFAULT_STYLES = {
+  :page_size   => 'A4',
+  :inline      => true,
+  :left_margin => 50
+}

597c1c065d877e34558d82e05bd0989518b802d3
Kevin Collignon <kevin@kevincollignon.com>
2012-04-16 00:59:23 -0600

    invoice validation and style updates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,6 +5,8 @@ class Invoice < ActiveRecord::Base
 	belongs_to :user
 	accepts_nested_attributes_for :items, :allow_destroy => true
 	
+	validates :title, :number, :presence => true
+	
 
 
   def total
